7fa08c9b4e962d9d30120670b78004682fefe013
renxin <renxin@baidu.com>
2015-02-26 18:02:02 +0800

    add files

---
+++ BaiduLibrary/BaiduLibrary/BaiduLibrary.h
@@ -0,0 +1,15 @@
+//
+//  BaiduLibrary.h
+//  BaiduLibrary
+//
+//  Created by zhuayi on 14/10/20.
+//  Copyright (c) 2014年 zhuayi inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "BaiduUtil.h"
+// app 宏
+#import "AppMacro.h"
+@interface BaiduLibrary : NSObject
+
+@end
